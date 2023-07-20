b447a9174e0fa5984e5398e3661abd2ac6aae7e3
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-07-20 00:05:38 -0500

    Added average changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,19 +7,31 @@ data_file = 'resources/budget_data.csv'
 csvfile = pd.read_csv(data_file)
 # Variables 
 sum = 0
+sum_of_changes = 0
+previous_profit_loss = 0
 
-#
+#loop through list
 for i, row in csvfile.iterrows():
     print (i)
-    print (row [1]) 
-    sum = sum + (row[1]) 
+    # print (row [1]) 
+    current_profit_loss = row[1]
+    sum = sum + current_profit_loss 
+
+    if i > 0:
+        change = current_profit_loss - previous_profit_loss
+        sum_of_changes = sum_of_changes + change
+        # print(change)
 
 
+    previous_profit_loss = current_profit_loss
+
+change_average = sum_of_changes / (len(csvfile)-1)
 #print header and number of rows of data
 print ("Financial Analysis")
 print ("----------------------------")
 print ("Total Months: " + str(len(csvfile)))
 print ("Total: $" + str(sum))
+print (f'Average Change: ${change_average:.2f}')
 
 
 
