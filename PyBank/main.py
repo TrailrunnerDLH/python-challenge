73d47d04ed982da040b591b5328c95aac4b26053
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-08-02 23:46:41 -0500

    Removed pandas

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,9 @@
-# Importing pandas lib
-import pandas as pd
+#Import CSV
+import csv
 #create variable to store file name
 data_file = 'resources/budget_data.csv'
 
-#read in csv file and store in variable
-csvfile = pd.read_csv(data_file)
-# Variables 
+# Create Variables to do calculations 
 sum = 0
 sum_of_changes = 0
 previous_profit_loss = 0
@@ -14,40 +12,60 @@ min_change = 0
 max_date = ""
 min_date = ""
 
+i=0
 
 #loop through list
-for i, row in csvfile.iterrows():
-    # print (i)
-    # print (row [1]) 
-    current_profit_loss = row[1]
-    sum = sum + current_profit_loss 
+with open (data_file, "r") as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+       #Skip header row
+        if i > 0:
+            #Append the current profit/loss to the sum
+            current_profit_loss = int(row[1])
+            sum = sum + current_profit_loss 
 
-    if i > 0:
-        change = current_profit_loss - previous_profit_loss
-        sum_of_changes = sum_of_changes + change
-        # print(change)
+            # Skip second row for the first line of data ( no change in previous period)
+            if i > 1:
+                #Calculate change for current period and append to sum of changes 
+                change = current_profit_loss - previous_profit_loss
+                sum_of_changes = sum_of_changes + change
 
-        if change > max_change:
-            max_change = change
-            max_date = row[0]
-        if change < min_change:
-            min_change = change
-            min_date = row[0]
+                #Loop through list and keep max change and date
+                if change > max_change:
+                    max_change = change
+                    max_date = row[0]
+                
+                #Loop through list and keep min change and date
+                if change < min_change:
+                    min_change = change
+                    min_date = row[0]
 
+            # store this row's profit loss for next iteration
+            previous_profit_loss = current_profit_loss
+        #increase index by one
+        i = i + 1
+#reduce index by one ( header). This equals the number of periods.
+count = i - 1
 
+#Divide sum of changes by the number of changes to get average of all changes
+change_average = sum_of_changes / (count-1)
 
-    previous_profit_loss = current_profit_loss
-
-change_average = sum_of_changes / (len(csvfile)-1)
-
-
-
-#print header and number of rows of data
+#print calculated values
 print ("Financial Analysis")
 print ("----------------------------")
-print ("Total Months: " + str(len(csvfile)))
+print ("Total Months: " + str(count))
 print ("Total: $" + str(sum))
 print (f'Average Change: ${change_average:.2f}')
 print("Greatest Increase in Profits: " + max_date  + " ($" + str(max_change)+")")
 print("Greatest Decrease in Profits: " + min_date  + " ($" + str(min_change)+")")
+
+#Print same values into text file
+with open( "analysis/analysis.txt", "w") as f:
+    print ("Financial Analysis", file=f)
+    print ("----------------------------", file=f)
+    print ("Total Months: " + str(count), file=f)
+    print ("Total: $" + str(sum), file=f)
+    print (f'Average Change: ${change_average:.2f}', file=f)
+    print("Greatest Increase in Profits: " + max_date  + " ($" + str(max_change)+")", file=f)
+    print("Greatest Decrease in Profits: " + min_date  + " ($" + str(min_change)+")", file=f)
 
